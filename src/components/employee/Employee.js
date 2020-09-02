bfb1332b1b76f9c999dfefb0894a9b2dc9a850dc
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-02 11:08:27 -0500

    got first kennel up and going

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Big Hal</h3>
+        <div className="employee__location">Nashville Kennels South</div>
+    </section>
+)
