785be3a84de1f6cb14bb9c02dfdba0e6944c2208
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-03 14:38:44 -0500

    rendering animals with proper owners

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider";
+import { CustomerContext } from "./CustomerProvider";
 import "./Animal.css"
 
 export const AnimalList = () => {
     const {animals, getAnimals} = useContext(AnimalContext)
+    const {customers, getCustomers} = useContext(CustomerContext)
 
     useEffect(() => {
             console.log(" **** animals before state pulled from API  ****")
-            getAnimals()
+            getAnimals().then(getCustomers)
         }, [])
 
     /*
@@ -15,19 +17,16 @@ export const AnimalList = () => {
             console.log(" **** animals after state pulled from API  ****")
         })
     */
-    useEffect(() => {
-       console.log(" **** animals after state pulled from API  ****")
-    }, [animals])
-
 
     return (
         <article className="animals">
             {
                 animals.map(animal => {
+                    const owner = customers.find(customer => customer.id === animal.customerId) || {}
                     return <section key={animal.id} className="animal">
                         <div><h3>{animal.name}</h3></div>
                         <div>{animal.breed}</div>
-                        <div>{animal.customerId}</div>
+                        <div>Owner: {owner.name}</div>
                         <div>{animal.locationId}</div>
                     </section>
                 })
